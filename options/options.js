0d57af9bda46328f8004a5b35bb3cfe85b9560d4
Hesbon Kiptoo <hesbon.kiptoo@crowdbotics.com>
2023-07-19 14:47:11 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "zutest-6",
-  url: "https://zutest-6.botics.co",
-  api: "https://zutest-6.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "zutest-6",
+  "url": "https://zutest-6.botics.co",
+  "api": "https://zutest-6.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "addBankAccount"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
